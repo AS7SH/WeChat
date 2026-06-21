438c68b179d33fbe7f7817b5965de53cf02acf80
asish <asishathota@gmail.com>
2026-06-21 17:27:18 +0530

    chore: remove old frontend to prepare for v2

--- frontend/src/components/Util/LoadingSpinner.jsx
+++
@@ -1,9 +0,0 @@
-const LoadingSpinner = () => {
-    return (
-        <div className="flex min-h-screen items-center justify-center">
-            <div className="h-8 w-8 animate-spin rounded-full border-2 border-border border-t-primary" />
-        </div>
-    );
-};
-
-export default LoadingSpinner;
